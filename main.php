0c22a3af2e81e6b6dfabb263ba01e678ec6fa88c
Alex <thesoftbytes@gmail.com>
2017-10-25 17:22:46 +1100

    Updated Main and Summary

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  }else{
 	$lang= "en";
  }
-
+include("bin/service.php");
 if(!isset($_GET["id"]) ){
 	//let service to handle request with no ID
 	$site = new siteService(null, $lang);
@@ -15,35 +15,40 @@ else $ethos_id= $_GET["id"];
 ?>
 
 <!doctype html>
-<html lang="<?= $lang ?>">
+<html lang="<?php echo $lang ?>">
 <?php
-
+	
 	require("helpers/langproc.php");
 	require("controller/ethosclient.php");
 	require("helpers/algos.php");
-
-
+	require("view/mainview.php");
+	  
+	//language controller class
 	$c = new getlangString($lang, "main");
 	$pagecontent = $c->getPageContent();
 	$pagetitle = $c->getPageTitle();
-
+	
+	 //ethOS Client API call
 	$o = new ethosClient($ethos_id, "summary");
 	$url= $o->getURL();
-	$o->sendRequest($url);
+	$o->sendRequest($url); 
 	$ethos_response =  $o->response_content;
 	$alldata= json_decode($ethos_response, true);
-
-
+	  
+	  
 //render html head section
 include("inc/head.php");
 ?>
 <body>
 <?php
 include("inc/topnav.php");
+
+//UI Summary Handler
+$s = new summaryBlock();
 include("inc/summary.php");
+	
 
-
-
+	
 ?>
 </body>
 </html>
