056b6a0ca23ef08cc4b268c533f340316bf8007a
Alex <thesoftbytes@gmail.com>
2017-10-25 17:24:55 +1100

    Updated summary view to use new view class and style

--- inc/summary.php
+++ inc/summary.php
@@ -1,31 +1,36 @@
 <div id="intro" class="jumbotron jumbotron-fluid text-center text-white">
-
+ 
  <?php
 
 	if($o->error ) echo "<h4>".$o->error_message."</h4>";
 	else{
 		?>
-  <h1><?= $pagecontent["summary"] ?></h1>
-  <h3><?= $pagecontent["sum_note"] ?></h3>
-
+  <h1><?php echo $pagecontent["summary"] ?></h1>
+  <h3><?php echo $pagecontent["sum_note"] ?></h3><br/>
+  
   <?php
-
+		
 		$summary = $alldata['per_info'];
 				foreach($summary as $algo_miner=>$sum_info){
 		?>
       <div class="summary_wrapper">
-        <div class="summary_block_<?= ($algo_miner) ?>">
-              <h3 class="card-title miner_summary_head_<?= $algo_miner; ?>"><?= get_algo($algo_miner) ?> </h3>
-              <h5>Total Hash: <?= $sum_info["hash"]." ".($algo_miner=='claymore'?'MH/s':'Sol/s'); ?></h5>
-              <h5>Total Rigs: <?= $sum_info["per_total_rigs"]; ?></h5>
-              <h5>Offline: <?= $sum_info["per_total_rigs"]-$sum_info["per_alive_rigs"]; ?></h5>
-              <span>Checked at: <?= date('H:i d/m', $sum_info["current_time"]); ?></span>
-            </div> <!-- block summary end -->
+        <div class="summary_block">
+              
+            <?php 
+					$s->summaryMiner($algo_miner, get_algo($algo_miner), $pagecontent["label1"]); 
+            		$s->summaryInfo($pagecontent["label2"], $sum_info["hash"], NULL , algoUnits(get_algo($algo_miner))); 
+            		$s->summaryInfo($pagecontent["label3"], $sum_info["per_alive_rigs"]."/".$sum_info["per_total_rigs"], $pagecontent["label4"]);
+					$s->summaryInfo($pagecontent["label5"], $sum_info["per_alive_gpus"]."/".$sum_info["per_total_gpus"], $pagecontent["label4"]);
+					$s->summaryInfo($pagecontent["label6"], date('H:i d/m', $sum_info["current_time"])); 
+			
+			?>
+              
+            </div> 
         </div>
-
-		<?php
+	    
+		<?php 
 		}
 	}
-
+	
 ?>
 
