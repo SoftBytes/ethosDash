61fed08da0d0d81d37193fa917d8ae2789fc5329
mobiusinfinity <hopibee@gmail.com>
2018-02-04 20:02:19 +1100

    Add files via upload

---
+++ model/coinRates.php
@@ -0,0 +1,16 @@
+	   <!--<div class="display-inline whitetext"><div class="coin_info">&nbsp;</div><div style="position: relative; top: 11px;" class="coin_info"><span class="stats_label">USD </span></div></div>-->
+   <?php
+   require("bittrex-client.php");//exchange
+	$callExchange=new bittrexClient();
+
+	$tradepairs=array("BTC"=>"USDT-BTC","ETH"=>"USDT-ETH","ZEC"=>"USDT-ZEC","XMR"=>"USDT-XMR");
+	   $tradevalues=array();
+
+	   foreach ($tradepairs as $pair=>$value){
+		   $call=$callExchange->sendRequest($value);
+		   $tradevalues = $callExchange->response_content;
+		   ?>
+	   <div id = "coin_rates" class="display-inline whitetext"><div class="coin_info"><img src="assets/images/<?php echo $pair; ?>.svg" width="30" style="text"></div><div class="coin_info"><?php echo round($tradevalues["result"][0]["Last"],1) ?>&nbsp;<img src="assets/images/<?php echo ($tradevalues["result"][0]["Last"]>=$tradevalues["result"][0]["PrevDay"]?"arrowup_green":"arrowdown_red") ?>.svg" height="12" style="text"><span class="stats_label display_75"><?php echo ($tradevalues["result"][0]["Last"]>=$tradevalues["result"][0]["PrevDay"]?round($tradevalues["result"][0]["Last"]/$tradevalues["result"][0]["PrevDay"]*100 -100,1):round(100-$tradevalues["result"][0]["Last"]/$tradevalues["result"][0]["PrevDay"]*100,1)) ?>%</span></div></div>
+		   <?php
+	   }
+	?>
