725a827ac6febd1ffb866e20b8c9525c9c51eaf4
mobiusinfinity <hopibee@gmail.com>
2018-02-04 21:34:13 +1100

    Add files via upload

--- inc/summary.php
+++ inc/summary.php
@@ -3,21 +3,21 @@ $powerbyAlgo= $stats->powerbyAlgo($alldata['rigs']);
 //hash summary by Algorithm
 $hashbyAlgo= $stats->sumbyAlgo($alldata['per_info']);
 $callWTM->wtmLink($hashbyAlgo,$powerbyAlgo);
-$callWTM->sendRequestwithParams(); 
+$callWTM->sendRequestwithParams();
 //echo $callWTM->url;
 
 ?>
 
 
 <div id = "intro" class="jumbotron jumbotron-fluid text-white text-center">
- 
+
  <?php
 	//validate wtm api call
 	if($callWTM->error || $callWTM->json_error){
 		$display_calculator = false;
 		echo $callWTM->error_message."<br/>";
 		echo $callWTM->url;
-	} 
+	}
 	else {
 		$display_calculator = true;
 		$response = $callWTM->response_content;
@@ -32,38 +32,38 @@ $callWTM->sendRequestwithParams();
 		//$powerbyAlgo= $stats->powerbyAlgo($alldata['rigs']);
 		//hash summary by Algorithm
 		//$hashbyAlgo= $stats->sumbyAlgo($alldata['per_info']);
-		
+
 	}
 	//get total power
 	  $sum_power = $stats->totalPower($alldata["rigs"]);
-	
+
 
 	//if($o->error ) echo "<h4>".$o->error_message."</h4>";
 	//else{
 
 		$tlabels = array("hashrate"=>$pagecontent["label7"],"gpus"=>$pagecontent["label5"],"total_alive" =>$pagecontent["label4"], "rigs"=>$pagecontent["label3"],"capacity"=>$pagecontent["label12"], "sum_power"=>$pagecontent["label17"]);
-		
-		$tvalues = array("hashrate"=>$alldata["total_hash"],"alive_gpus"=>$alldata["alive_gpus"],"total_gpus" =>$alldata["total_gpus"], "alive_rigs"=>$alldata["alive_rigs"],"total_rigs"=>$alldata["total_rigs"],"capacity"=>$alldata["capacity"], "sum_power"=>$sum_power);
-		
-		
-	?>	
-<!-- Tital Hash, Capacity, Ris and GPus -->
-<div class="summary_wrapper">
 
-<?php 
+		$tvalues = array("hashrate"=>$alldata["total_hash"],"alive_gpus"=>$alldata["alive_gpus"],"total_gpus" =>$alldata["total_gpus"], "alive_rigs"=>$alldata["alive_rigs"],"total_rigs"=>$alldata["total_rigs"],"capacity"=>$alldata["capacity"], "sum_power"=>$sum_power);
+
+
+	?>
+<!-- Tital Hash, Capacity, Ris and GPus -->
+<div class="summary_wrapper" id="">
+
+<?php
 		if($display_calculator){
 		?>
 		<div class="summary_block">
-		<?php 
-		//echo $s->displayMilestonePeriod($calculator->get1BTCdays($calculator->coininfo, $hashbyAlgo, $calculator->defaults), $pagecontent["forecast"],$pagecontent["1btcforecast"]); 
-		echo $s->displayMilestonePeriod($calculator->get1BTCdays2($calculator->coininfo, $hashbyAlgo), $pagecontent["forecast"],$pagecontent["1btcforecast"]); 
+		<?php
+		//echo $s->displayMilestonePeriod($calculator->get1BTCdays($calculator->coininfo, $hashbyAlgo, $calculator->defaults), $pagecontent["forecast"],$pagecontent["1btcforecast"]);
+		echo $s->displayMilestonePeriod($calculator->get1BTCdays2($calculator->coininfo, $hashbyAlgo), $pagecontent["forecast"],$pagecontent["1btcforecast"]);
 		?>
 		</div>
-		<?php	
+		<?php
 		}
 	?>
 		<div class="summary_block">
-	<?php 
+	<?php
 		$s->totalInfo($tvalues, $tlabels) ;
 		//$s->sumhashInfo($stats->sumbyAlgo($alldata['per_info']));
 	?>
@@ -71,17 +71,17 @@ $callWTM->sendRequestwithParams();
 </div>
 
 	<?php
-	
+
 	//$s->sumhashInfo($stats->sumbyAlgo($alldata['per_info']));
 	if($display_calculator){
-			
-					
+
+
 ?>
-				<div class="summary_wrapper">
+				<div class="summary_wrapper" id="topCoins">
 				<div class="summary_block">
 				<h5><?php echo $pagecontent["calculator"]; ?></h5>
-				
-				<?php			
+
+				<?php
 				//$s->totalInfo($tvalues, $tlabels) ;
 			foreach($stats->sumbyAlgo($alldata['per_info']) as $algo=>$hash){
 				//echo "<div style=\"text-align:left;\"><span>".$algo."</span><br/>";
@@ -93,14 +93,14 @@ $callWTM->sendRequestwithParams();
 			}
 			echo $s->wtmLink($hashbyAlgo, $powerbyAlgo, $pagecontent["poweredby"], $pagecontent["wtmlinknote"]);
 					?>
-					
+
 					</div></div>
 		<?php
 		//echo "CURL Error: ".$callapi->error_message." error code:".$callapi->error_code."<br/>";
 		//echo "Cannot get data for coins info.";
-			
-			
-			
+
+
+
 	}else{
 		//echo "Respones status code:".$callapi->curl_status_code;
 
@@ -113,25 +113,25 @@ $callWTM->sendRequestwithParams();
 
 				$s->displayHashStats($algo,$hash);
 	}
-	
+
 	?>
 </div>
 <div class="summary_block">
 	<h5><?php echo $pagecontent["label21"]; ?></h5>
-	<?php 
+	<?php
 
 		$miners_summary = $alldata['per_info'];//information from miners as per_info
 		$pools_stats=$stats->minerPools($alldata['rigs']);
-		//$miners_details = 
+		//$miners_details =
 		foreach($miners_summary as $algo_miner=>$sum_info){
-		
-					$s->summaryMiner($algo_miner, get_algo($algo_miner), $pagecontent["label21"]); 
+
+					$s->summaryMiner($algo_miner, get_algo($algo_miner), $pagecontent["label21"]);
 					$s->minerPool($pools_stats[$algo_miner]);
 		}
 		?>
 	</div>
 
-	
+
 </div>
 
 </div>
